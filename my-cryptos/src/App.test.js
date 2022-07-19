e407db52e721e3fc58fcfb32cd41384f3ace6390
fjreyesm <franklin.reyes.2003@gmail.com>
2022-07-19 22:42:44 +0200

    Test fetch OK

--- my-cryptos/src/App.test.js
+++ my-cryptos/src/App.test.js
@@ -1,13 +1,26 @@
-import App from "./App";
-import { render, screen } from "@testing-library/react";
+import React, { Component } from "react";
+import { render, screen, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import renderer from "react-test-renderer";
+import fetch from "node-fetch";
+import Header from "./components/Header";
 
+/* import CoinsRow from "./components/CoinsRow";
+render(<CoinsRow />);
 test("should verify h1 displays correct text", () => {
-  render(<App />);
-  const h1 = screen.getByText(/coin/i);
-  expect(h1).toBeInTheDocument();
-});
+  const p = screen.getByText(/coin/i);
+  expect(p).toBeInTheDocument();
+}); */
 
+render(<Header />);
+
+let url = "https://api.coingecko.com/api/v3/coins/list";
 test("should sum coins", () => {
   const suma = (a, b) => a + b;
   expect(suma(1, 2)).toBe(3);
+});
+
+test("should return API response with status 200", async () => {
+  const response = await fetch(url);
+  expect(response.status).toBe(200);
 });
--- my-cryptos/src/components/CoinsRow.test.js
+++ my-cryptos/src/components/CoinsRow.test.js
@@ -1,3 +1,6 @@
+import { render, screen, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
 test("prueba suma", () => {
   const suma = (a, b) => a + b;
   expect(suma(1, 2)).toBe(3);
